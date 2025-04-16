1a860835137457674c22197c6b90cdea3f1451ab
Lubos V. <tridosm@gmail.com>
2025-04-16 17:49:53 +0200

    Enhanced PBR shader

--- app/src/main/jni/d3es-multithread-master/neo/renderer/glsl/interactionShaderFP.cpp
+++ app/src/main/jni/d3es-multithread-master/neo/renderer/glsl/interactionShaderFP.cpp
@@ -94,6 +94,11 @@ void main()
   vec3 lightFalloff = texture(u_fragmentMap1, vec2(var_TexLight.z, 0.5)).rgb;
   vec3 diffuseColor = texture(u_fragmentMap3, var_TexDiffuse).rgb * u_diffuseColor.rgb;
 
+  //Lubos: make diffuse color less present when applying PBR
+  {
+    diffuseColor = diffuseColor * 0.67 + u_diffuseColor.rgb * 0.33;
+  }
+
   //PBR
   vec3 AN = normalize(mix(normalize(var_Normal), N, 1.0));
   vec4 Cd = vec4(diffuseColor.rgb, 1.0);
@@ -121,8 +126,8 @@ void main()
   vec3 color;
   color = diffuseColor;
   //Lubos BEGIN
+  float scale = 4.0;
   float smoothing = 0.33;
-  float scale = u_specularExponent + u_specularExponent;
   color.r += u_specularColor.r * pow(pbr.r, smoothing) * scale;
   color.g += u_specularColor.g * pow(pbr.g, smoothing) * scale;
   color.b += u_specularColor.b * pow(pbr.b, smoothing) * scale;
@@ -131,5 +136,15 @@ void main()
   color *= lightFalloff;
 
   fragColor = vec4(color, 1.0) * var_Color;
+
+  //Lubos:saturize the final color
+  {
+    float gray = (fragColor.r + fragColor.g + fragColor.b) / 3.0;
+    fragColor.r += (fragColor.r - gray) * 0.25;
+    fragColor.g += (fragColor.g - gray) * 0.25;
+    fragColor.b += (fragColor.b - gray) * 0.25;
+    fragColor.rgb -= vec3(gray, gray, gray) * 0.5;
+    fragColor.rgb *= 0.75;
+  }
 }
 )";
